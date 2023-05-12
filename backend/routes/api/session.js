678278cdaa579c9bc5c12df0ff6b147f31d3aade
Peter Dinh <314pdinh@gmail.com>
2023-05-11 20:09:40 -0400

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,24 +1,30 @@
-// backend/routes/api/session.js
 const express = require('express')
-const router = express.Router();
-
-// backend/routes/api/session.js
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
-
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
-
+const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 // backend/routes/api/session.js
 // ...
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
-  
       const user = await User.unscoped().findOne({
         where: {
           [Op.or]: {
@@ -27,7 +33,6 @@ router.post(
           }
         }
       });
-  
       if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
         const err = new Error('Login failed');
         err.status = 401;
@@ -35,27 +40,18 @@ router.post(
         err.errors = { credential: 'The provided credentials were invalid.' };
         return next(err);
       }
-  
       const safeUser = {
         id: user.id,
         email: user.email,
         username: user.username,
       };
-  
       await setTokenCookie(res, safeUser);
-  
       return res.json({
         user: safeUser
       });
     }
   );
-
-
-
-  // backend/routes/api/session.js
-// ...
-
-// Log out
+  // Log out
 router.delete(
     '/',
     (_req, res) => {
@@ -63,30 +59,21 @@ router.delete(
       return res.json({ message: 'success' });
     }
   );
-  
-  // ...
-
-
-  // backend/routes/api/session.js
-// ...
-
-// Restore session user
+  // Restore session user
 router.get(
-  '/',
-  (req, res) => {
-    const { user } = req;
-    if (user) {
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
-      return res.json({
-        user: safeUser
-      });
-    } else return res.json({ user: null });
-  }
-);
-
-// ...
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
 module.exports = router;
